184aebdb436f5b036aa29394aa810a50734be50c
hjj <646562345@qq.com>
2016-06-27 19:55:12 +0800

    basic mothod

---
+++ app/src/main/java/imdemo/bmob/cn/myapplication/MainActivity.java
@@ -0,0 +1,51 @@
+package imdemo.bmob.cn.myapplication;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //创建侧滑菜单
+        SlidingMenu menu = new SlidingMenu(this);
+        menu.setMode(SlidingMenu.LEFT);
+        menu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+        menu.setFadeDegree(0.55f);
+        menu.setMenu(R.layout.menu_layout);
+        menu.setBackgroundColor(Color.LTGRAY);
+        menu.setBehindScrollScale(1.0f);
+        menu.setBehindOffset(R.dimen.menu_offset);
+        menu.attachToActivity(this,SlidingMenu.SLIDING_CONTENT);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
